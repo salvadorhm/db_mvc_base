4924525f081ea4a584d34d32bcfb702b499c0388
Salvador HM <salvadorhm@utec-tgo.edu.mx>
2018-10-01 00:47:26 -0500

    ModelAgenda metodo Conectar implementado exitosamente

--- src/models/ModelAgenda.java
+++ src/models/ModelAgenda.java
@@ -5,10 +5,54 @@
  */
 package models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Zeo
  */
 public class ModelAgenda {
-    
+
+    private Connection conexion;
+    private Statement st;
+    private ResultSet rs;
+
+    private String nombre;
+    private String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void conectarDB() {
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3309/agenda_mvc", "user_mvc", "pass_mvc.2018");
+            st = conexion.createStatement();
+            rs = st.executeQuery("SELECT * FROM contactos;");
+            rs.next();
+            nombre = rs.getString("nombre");
+            email = rs.getString("email");
+        } catch (SQLException err) {
+            JOptionPane.showMessageDialog(null, "Error ModelAgenda 001: " + err.getMessage());
+        }
+
+    }
+
 }
