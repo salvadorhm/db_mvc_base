26dabb55e152b404a909d8a5538b8ee3d3140bbf
Salvador HM <salvadorhm@utec-tgo.edu.mx>
2018-10-01 00:33:49 -0500

    ControllerAgenda con ActionListner implementado, y la esctructura de los métodos Primero, Siguiente, Anterior y Ultimo implementados.

--- src/controllers/ControllerAgenda.java
+++ src/controllers/ControllerAgenda.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import models.ModelAgenda;
 import views.ViewAgenda;
 
@@ -12,18 +15,88 @@ import views.ViewAgenda;
  * @author Zeo
  */
 public class ControllerAgenda {
+
     ModelAgenda modelAgenda;
     ViewAgenda viewAgenda;
 
+    /**
+     * Objeto de tipo ActionListener para atrapar los eventos actionPerformed y
+     * llamar a los metodos para ver los registros almacenados en la bd.
+     */
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == viewAgenda.jbtn_primero) {
+                jbtn_primero_actionPerformed();
+            } else if (e.getSource() == viewAgenda.jbtn_anterior) {
+                jbtn_anterior_actionPerformed();
+            } else if (e.getSource() == viewAgenda.jbtn_siguiente) {
+                jbtn_siguiente_actionPerformed();
+            } else if (e.getSource() == viewAgenda.jbtn_ultimo) {
+                jbtn_ultimo_actionPerformed();
+            }
+
+        }
+
+    };
+
+    /**
+     * Constructor de Controlador para unir el ModelAgenda y ViewAgenda
+     *
+     * @param modelAgenda objeto de tipo ModelAgenda
+     * @param viewAgenda objeto de tipo ViewAgenda
+     */
     public ControllerAgenda(ModelAgenda modelAgenda, ViewAgenda viewAgenda) {
         this.modelAgenda = modelAgenda;
         this.viewAgenda = viewAgenda;
         initComponents();
+        setActionListener();
     }
-    
-    public void initComponents(){
+
+    /**
+     * Metodo para inicializar la ViewAgenda
+     */
+    public void initComponents() {
         viewAgenda.setLocationRelativeTo(null);
         viewAgenda.setTitle("Agenda MVC");
         viewAgenda.setVisible(true);
+    }
+
+    /**
+     * Método para agregar el actionListener a cada boton de la vista
+     */
+    public void setActionListener() {
+        viewAgenda.jbtn_primero.addActionListener(actionListener);
+        viewAgenda.jbtn_anterior.addActionListener(actionListener);
+        viewAgenda.jbtn_siguiente.addActionListener(actionListener);
+        viewAgenda.jbtn_ultimo.addActionListener(actionListener);
+    }
+
+    /**
+     * Método para ver el primer registro de la tabla contactos
+     */
+    private void jbtn_primero_actionPerformed() {
+        System.out.println("Action del boton jbtn_primero");
+    }
+
+    /**
+     * Método para ver el registro anterior de la tabla contactos
+     */
+    private void jbtn_anterior_actionPerformed() {
+        System.out.println("Action del boton jbtn_anterior");
+    }
+
+    /**
+     * Método para ver el último registro de la tabla contactos
+     */
+    private void jbtn_ultimo_actionPerformed() {
+        System.out.println("Action del boton jbtn_ultimo");
+    }
+
+    /**
+     * Método para ver el siguiente registro de la tabla contactos
+     */
+    private void jbtn_siguiente_actionPerformed() {
+        System.out.println("Action del boton jbtn_siguiente");
     }
 }
